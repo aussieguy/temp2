fd3f8b9c0942841b7c4b3f501d27367b85fa4ffe
Glen Hobby <glen.hobby@visionaustralia.org>
2015-05-12 17:49:47 +1000

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,6 @@ ActiveRecord::Schema.define(version: 20150406043814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "name"
-    t.date     "release"
-    t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "country"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
